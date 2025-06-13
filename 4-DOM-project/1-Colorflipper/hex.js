b26cd87a5983e70c3ac7978d67be6906eec59356
sandhyashiwakoti <158501166+sandhyashiwakoti@users.noreply.github.com>
2025-06-13 22:38:14 +0545

    feat: add reviews DOM project

--- 4-DOM-project/1-Colorflipper/hex.js
+++ 4-DOM-project/1-Colorflipper/hex.js
@@ -1,17 +1,16 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 const color = document.querySelector(".color");
 
-
 btn.addEventListener("click", (e) => {
-    let randomColor = "#";
-    for(let i = 0; i < 6; i++){
-      randomColor += hex[Math.floor(Math.random() * hex.length)];
-    }
-    color.innerText = randomColor;
-    color.style.color = randomColor;
-    container.style.backgroundColor = randomColor;
-    console.log(randomColor);
+  let randomColor = "#";
+  for (let i = 0; i < 6; i++) {
+    randomColor += hex[Math.floor(Math.random() * hex.length)];
+  }
+  color.innerText = randomColor;
+  color.style.color = randomColor;
+  container.style.backgroundColor = randomColor;
+  console.log(randomColor);
 });
