139dc159a7606bf51f8ccca35b47e2836fdeaf0e
sandhyashiwakoti <158501166+sandhyashiwakoti@users.noreply.github.com>
2025-06-25 22:43:03 +0545

    feat: add event object file

---
+++ 3-DOM/23-event-object/script.js
@@ -0,0 +1,17 @@
+const heading = document.querySelector("h1");
+const btn = document.querySelector(".btn");
+const link = document.getElementById("link");
+
+heading.addEventListener("click", (event) => {
+  console.log(event.currentTarget);
+  console.log(this);
+});
+btn.addEventListener("click", function (event) {
+  event.currentTarget.classList.add("blue");
+  console.log(event.type);
+});
+
+function someFunc(e) {
+  e.preventDefault();
+}
+link.addEventListener("click", someFunc);
