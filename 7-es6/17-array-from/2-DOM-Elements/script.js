f8a06fa8b4f00d1448b13a3fd79ecbb5910b1409
sandhyashiwakoti <158501166+sandhyashiwakoti@users.noreply.github.com>
2025-07-03 00:16:15 +0545

    feat: update array-from-DOM file

---
+++ 7-es6/17-array-from/2-DOM-Elements/script.js
@@ -0,0 +1,14 @@
+const p = document.querySelectorAll("p");
+const result = document.getElementById("result");
+const second = document.getElementById("second");
+
+let newText = Array.from(p);
+newText = newText.map((item) => "<span>${item. textContent}</span>").join(" ");
+
+result.innerHTML = newText;
+
+const text = Array.from(document.querySelectorAll("p"), (item) => {
+  return `<span>${item.textContent}</span>`;
+}).join(" ");
+
+second.innerHTML = text;
